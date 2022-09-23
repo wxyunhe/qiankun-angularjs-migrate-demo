7f398ea78709f0a850324f3626fd5c298aaa8826
liyunhe <liyunheasap@yeah.net>
2022-09-23 15:00:54 +0800

    feat: 目录调整

---
+++ packages/app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <main id="subapp-container">
+      <div id="subapp-container">
+        <div id="subapp-viewport"></div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
